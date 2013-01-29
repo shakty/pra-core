d66d9a95044ba2cb6f1470ed44fc49c631453b9a
Stefano Balietti <futur.dorko@gmail.com>
2013-01-29 10:55:12 +0100

    refactored..

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ var core = require('./lib/basic'),
 	dist = require('./lib/dist'),
 	getfaces = require('./lib/getfaces'),
 	cordis = require('./lib/correlate_distance'),
+	round = require('./lib/round'),
 	dist2 = require('./lib/dist_advanced');
 	
 
 J.mixin(core, feats);
-J.mixin(core, dist);
 J.mixin(core, getfaces);
 J.mixin(core, cordis);
 J.mixin(core, dist2);
+J.mixin(core, round);
+
+core.round = round;
+core.dist = dist;
 
 module.exports = core;
