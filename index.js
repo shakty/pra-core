44bde423eb3d403c59f980dbd29fdb3939890dad
Stefano Balietti <futur.dorko@gmail.com>
2013-01-29 10:38:59 +0100

    refactored

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./lib/pr_stats');
+var J = require('JSUS').JSUS;
+
+var core = require('./lib/basic'),
+	feats = require('./lib/features'),
+	dist = require('./lib/dist'),
+	getfaces = require('./lib/getfaces'),
+	cordis = require('./lib/correlate_distance'),
+	dist2 = require('./lib/dist_advanced');
+	
+
+J.mixin(core, feats);
+J.mixin(core, dist);
+J.mixin(core, getfaces);
+J.mixin(core, cordis);
+J.mixin(core, dist2);
+
+module.exports = core;
