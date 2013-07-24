041de046f0cc7407cf6a8755fd7b0df9cea7f2df
Stefano Balietti <futur.dorko@gmail.com>
2013-07-24 15:51:33 +0200

    separated in a new file the functions for working with R files

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var J = require('JSUS').JSUS;
 
 var core = require('./lib/basic'),
-	feats = require('./lib/features'),
-	dist = require('./lib/dist'),
-	getfaces = require('./lib/getfaces'),
-	cordis = require('./lib/correlate_distance'),
-	round = require('./lib/round'),
-	dist2 = require('./lib/dist_advanced');
-	
+feats = require('./lib/features'),
+dist = require('./lib/dist'),
+getfaces = require('./lib/getfaces'),
+cordis = require('./lib/correlate_distance'),
+round = require('./lib/round'),
+dist2 = require('./lib/dist_advanced'),
+R = require('./lib/r');
+
 
 J.mixin(core, feats);
 J.mixin(core, getfaces);
 J.mixin(core, cordis);
 J.mixin(core, dist2);
 J.mixin(core, round);
+J.mixin(core, R);
 
 core.round = round;
 core.dist = dist;
+core.R = R;
 
 module.exports = core;
